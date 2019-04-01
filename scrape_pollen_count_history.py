dfd0cc53ef79b31e508aee0e6b98f0ffd5c52fa6
Kelly Gilbert <gilbert.kelly.lynn@gmail.com>
2019-03-31 23:08:15 -0400

    continued development on scrape_pollen_count_history.py

--- scrape_pollen_count_history.py
+++ scrape_pollen_count_history.py
@@ -24,13 +24,13 @@ current_date = dt.strptime(start_date, '%Y-%m-%d')
 end_date = dt.strptime(end_date, '%Y-%m-%d')    # convert to datetime
 
 # initialize the lists
-error_dates = []
-error_types = []
+error_list = []
 results = []
 
 
 while current_date <= end_date:
-
+    print('Starting ' + str(current_date))
+        
     # send the get request with the current date
     # the user agent is set to avoid blocking; there is no specific reason for
     # using 'custom' (it could be any string)
@@ -39,56 +39,71 @@ while current_date <= end_date:
 
     # if the request was not successful, log an error
     if r.status_code != 200:
-        error_dates.append(start_date)
-        error_types.append('Request status: ' + str(r.status_code) + ' - ' 
-                           + r.reason) 
+        error_list.append( { 'date' : current_date,
+                             'type' : 'Request status: ' + str(r.status_code) \
+                             + ' - ' + r.reason } )
     else:
         # convert result text to a soup object
         soup = BeautifulSoup(r.text, 'lxml')
 
         # get the overall count
         daily_count = soup.body.find(class_='pollen-num')
-        daily_count = daily_count.text.strip()
-        print(daily_count)
+        if not daily_count:    # no pollen count available
+            error_list.append( { 'date' : current_date,
+                                 'type' : 'Pollen count not available' } )                        
+        else:
+            daily_count = daily_count.text.strip()
+#           print(daily_count)
         
-        # get the contributors
-        category_list = []
-        for g in soup.find_all(class_='gauge'):         
-            # get the category (trees, weeds, etc.)
-            category_name = g.h5.text    # return text in h5 tag
-            category_name = re.match('^(\w)+', category_name)[0]    # first word
-#            print(category_name)
+            # get the contributors
+            category_list = []
+            for g in soup.find_all(class_='gauge'):         
+                # get the category (trees, weeds, etc.)
+                category_name = g.h5.text    # return text in h5 tag
+                category_name = re.match('^(\w)+', category_name)[0]    # first word
+#                print(category_name)
 
-            # get the list of types (sycamore, etc.)
-            category_types = g.p.text.strip()
-#            print(category_types)
+                # get the list of types (sycamore, etc.)
+                category_types = g.p.text.strip()
+#                print(category_types)
 
-            # get the severity value (0 - 100%)          
-            category_amount = g.find(class_='needle')['style']
-            category_amount = re.match('.*?([\d\.+]+)%', category_amount)[1]
-#            print(category_amount)
- 
-            # create a dictionary of detailed results
-            category_dict = { 'category_name' : category_name,
-                              'category_types' : category_types, 
-                              'category_amount' : category_amount }
-            category_list.append(category_dict)
-#            print(category_list)
+                # get the severity value (0 - 100%)          
+                category_amount = g.find(class_='needle')['style']
+                category_amount = re.match('.*?([\d\.+]+)%', category_amount)[1]
+#                print(category_amount)
+                
+                # create a dictionary of detailed results
+                category_dict = { 'category_name' : category_name,
+                                 'category_types' : category_types, 
+                                 'category_amount' : category_amount }
+                category_list.append(category_dict)
+#                print(category_list)
+                
+            # create a dictionary of result details
+            results_dict = { 'date' : current_date,
+                            'daily_count' : daily_count, 
+                            'category_list' : category_list }
+                
+                
+            # add the daily results to the results list
+            results.append(results_dict)
         
-        # create a dictionary of result details
-        results_dict = { 'date' : current_date,
-                         'daily_count' : daily_count, 
-                         'category_list' : category_list }
-        
-    
-        # add the daily results to the results list
-        results.append(results_dict)
-
         # increment the date
         current_date += datetime.timedelta(days=1)
 #        print(current_date)
 
-print(error_dates)
-print(error_types)
+
+
+
+# results for debugging:
+print(error_list)
 for i in results:
     print(i)
+    
+# write results to file
+    
+    
+    
+    
+    
+    
